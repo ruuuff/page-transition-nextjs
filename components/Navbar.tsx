9b2fe6552e691bb3d89e91a42da982bfaa1c92ed
Carlos Daniel <ruuufffaria@gmail.com>
2023-01-04 15:04:56 -0300

    colors transition

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import { motion } from "framer-motion";
 
-export default function Navbar({ currentPath }: { currentPath: string }) {
+interface Props {
+  currentPath: string;
+}
+
+export default function Navbar({ currentPath }: Props) {
   const paths = [
     { name: "Home", url: "/" },
     { name: "Contact", url: "/contact" },
@@ -11,7 +15,7 @@ export default function Navbar({ currentPath }: { currentPath: string }) {
   return (
     <nav className="text-2xl font-medium px-4 py-12 absolute left-0 right-0 top-0 z-20">
       <div className="max-w-6xl mx-auto">
-        <ul className="flex gap-12">
+        <ul className="flex gap-12 transition-colors duration-200">
           {paths.map(({ name, url }) => (
             <li key={name} className="relative">
               <Link href={url}>{name}</Link>
