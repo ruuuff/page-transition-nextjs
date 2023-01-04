cb4658ebfb11549b5f5eb0ee37da91424069ff1f
Carlos Daniel <ruuufffaria@gmail.com>
2023-01-04 15:09:27 -0300

    feat: text color based on url path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
@@ -12,9 +13,25 @@ const lato = Lato({
   variable: "--font-lato",
 });
 
+const textColors: { [key: string]: string } = {
+  default: "text-gray-900",
+  "/contact": "text-red-50",
+  "/about": "text-white",
+};
+
 export default function App({ Component, pageProps, router }: AppProps) {
+  const pathname = router.pathname;
+  const color = textColors[pathname] || textColors.default;
+  const [textColor, setTextColor] = useState(color);
+
+  useEffect(() => {
+    setTimeout(() => setTextColor(color), 400);
+  }, [pathname]);
+
+  console.log("render");
+
   return (
-    <div className={`${lato.variable} font-lato text-gray-900`}>
+    <div className={`${lato.variable} font-lato ${textColor}`}>
       <Head>
         <title>Page Transition - ruuuff</title>
         <meta
@@ -25,10 +42,10 @@ export default function App({ Component, pageProps, router }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar currentPath={router.pathname} />
+      <Navbar currentPath={pathname} />
 
       <AnimatePresence initial={false}>
-        <Component key={router.pathname} {...pageProps} />
+        <Component key={pathname} {...pageProps} />
       </AnimatePresence>
     </div>
   );
