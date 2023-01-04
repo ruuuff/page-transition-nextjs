0a983c1e27354cf39620674601e67ce4c6c4372a
Carlos Daniel <ruuufffaria@gmail.com>
2023-01-04 15:46:19 -0300

    rename property "url" to "path" on paths array

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,21 +5,21 @@ interface Props {
   currentPath: string;
 }
 
-export default function Navbar({ currentPath }: Props) {
-  const paths = [
-    { name: "Home", url: "/" },
-    { name: "Contact", url: "/contact" },
-    { name: "About", url: "/about" },
-  ];
+const paths = [
+  { name: "Home", path: "/" },
+  { name: "Contact", path: "/contact" },
+  { name: "About", path: "/about" },
+];
 
+export default function Navbar({ currentPath }: Props) {
   return (
     <nav className="text-2xl font-medium px-4 py-12 absolute left-0 right-0 top-0 z-20">
       <div className="max-w-6xl mx-auto">
         <ul className="flex gap-12 transition-colors duration-200">
-          {paths.map(({ name, url }) => (
+          {paths.map(({ name, path }) => (
             <li key={name} className="relative">
-              <Link href={url}>{name}</Link>
-              {currentPath === url && (
+              <Link href={path}>{name}</Link>
+              {currentPath === path && (
                 <motion.div
                   layoutId="underline"
                   className="absolute bottom-0 h-[2px] bg-current w-full"
